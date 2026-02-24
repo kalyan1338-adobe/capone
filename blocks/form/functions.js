3d9f447296b1faa1185a9883f0377ff0127bcbad
Korada Kalyanchakravarthi <korada.k@ad.infosys.com>
2026-02-24 12:40:31 +0530

    exponential

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -59,7 +59,7 @@ function maskMobileNumber(mobileNumber) {
 /* emi calculation */
 function emical(loan,interest,tenure){
   const r = interest/(12 * 100);
-  const emi=(loan*r*(1+r)^tenure)/((1+r)^tenure)-1;
+  const emi=(loan*r*(1+r)**tenure)/((1+r)**tenure)-1;
   const final_emi=Math.trunc(emi).toLocaleString('en-IN');
   return final_emi;
 }
